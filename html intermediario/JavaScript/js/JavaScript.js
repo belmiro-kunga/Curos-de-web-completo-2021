7828cea6592acd0a328cea8781eef9405dda9fc8
belmiro-kunga <belmirokunga@gmail.com>
2021-04-17 02:35:18 +0100

    html semantico

---
+++ html intermediario/JavaScript/js/JavaScript.js
@@ -0,0 +1,5 @@
+function myfun(){
+    document.getElementById("demostracao").style.fonsize = "25px";
+    document.getElementById("demostracao").style.color ="red";
+    document.getElementById("demostracao").style.backgroundColor="yellow";
+}
